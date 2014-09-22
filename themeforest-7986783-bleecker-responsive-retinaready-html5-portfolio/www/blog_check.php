1c803cd06e30e099fa2ac45573ea4dea96cac824
gderouineau <admin@caradoc.fr>
2014-09-22 14:08:20 +0200

    ajout fichier actualité.js permettant l'affichage des actualités vers ajax

--- themeforest-7986783-bleecker-responsive-retinaready-html5-portfolio/www/blog_check.php
+++ themeforest-7986783-bleecker-responsive-retinaready-html5-portfolio/www/blog_check.php
@@ -4,4 +4,30 @@
  * User: GD
  * Date: 19/09/2014
  * Time: 13:25
- */ 
+ */
+
+if(isset($_POST['title']) && isset($_POST['text'])){
+
+    $photo = "";
+    if(isset($_POST['photo'])){
+        $photo = $_POST['photo'];
+    }
+    $date = $_POST['date'];
+    $title = $_POST['title'];
+    $text = $_POST['text'];
+
+    try{
+        $db = new PDO('mysql:host=mysql51-136.perso;dbname=jordandefmbdd', 'jordandefmbdd', 'hgz5pTRuktht');
+    }
+    catch (PDOException $e) {
+        echo "Erreur !: " . $e->getMessage() . "<br/>";
+        die();
+    }
+    $query='INSERT INTO newsletter VALUES (null,\''.$email.'\')';
+    $insert = $db->query($query);
+    echo 'success';
+
+}
+else{
+    echo 'missing data';
+}
