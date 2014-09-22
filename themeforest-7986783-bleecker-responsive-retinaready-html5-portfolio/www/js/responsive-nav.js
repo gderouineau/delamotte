5d8b75509824cf89ee17d7425f30af6fe680a9dc
gderouineau <admin@caradoc.fr>
2014-09-22 14:32:13 +0200

    responsive nav

--- themeforest-7986783-bleecker-responsive-retinaready-html5-portfolio/www/js/responsive-nav.js
+++ themeforest-7986783-bleecker-responsive-retinaready-html5-portfolio/www/js/responsive-nav.js
@@ -7,12 +7,19 @@ jQuery(document).ready(function(){
 	"use strict";
 	
 	jQuery('#nav-button').click(function() {
-			jQuery('#options').slideFadeToggle();
+			jQuery('#options,#options-down').slideFadeToggle();
 	});
-	
+
+    if ( jQuery(window).width() < 767){
+        var up = jQuery('#option-list-up').html();
+        var down = jQuery('#option-list-down').html();
+
+        jQuery('#option-list-up').html(up+down);
+    }
+
 	if ( jQuery(window).width() < 767) {
 	jQuery('#options li a').click(function() {
-			jQuery('#options').hide();
+			jQuery('#options,#options-down').hide();
 	});
 	}
 	
