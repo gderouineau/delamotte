72f34f2addded226a8efb3673f9e246d67cb6411
jordandelamotte <chef.jordandelamotte@gmail.com>
2014-12-19 15:40:11 +0100

    [update] : divers

--- themeforest-7986783-bleecker-responsive-retinaready-html5-portfolio/www/newsletter/janvier2014/send.php
+++ themeforest-7986783-bleecker-responsive-retinaready-html5-portfolio/www/newsletter/janvier2014/send.php
@@ -4,4 +4,19 @@
  * User: jordandelamotte
  * Date: 19/12/2014
  * Time: 15:28
- */ 
+ */
+$id=1;
+$email = "chefjordandelamotte@gmail.com";
+
+$html = file_get_contents("newsletter.html");
+$html = str_replace("##id##",$id,$html);
+
+
+$to=$email;
+$subject = "Newsletter - jordan-delamotte.com";
+$message = $html;
+$headers = 'From: contact@jordan-delamotte.com' . "\r\n" .
+    'Reply-To: contacct@jordan-delamotte.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+mail($to, $subject, $message, $headers);
